58930b313c09be0f9f52870855cf7bf14ad4051d
James Horton <jameshorton@gmail.com>
2018-11-12 15:24:36 -0500

    Updates

---
+++ precache-manifest.524c3ea315ab04549335e1264d1a5422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17bf4564c6f97911fa43",
+    "url": "/github-cards/static/js/runtime~main.17bf4564.js"
+  },
+  {
+    "revision": "0fda690525c226e806f6",
+    "url": "/github-cards/static/js/main.0fda6905.chunk.js"
+  },
+  {
+    "revision": "fceaa1fd06956054045f",
+    "url": "/github-cards/static/js/1.fceaa1fd.chunk.js"
+  },
+  {
+    "revision": "0fda690525c226e806f6",
+    "url": "/github-cards/static/css/main.b7950a31.chunk.css"
+  },
+  {
+    "revision": "7b3418847baeb1bf5647a7cc816c2963",
+    "url": "/github-cards/index.html"
+  }
+];
