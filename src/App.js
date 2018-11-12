ddb80324e83a46a8d78b7ea31f2e4e63047e180e
James Horton <jameshorton@gmail.com>
2018-11-12 14:16:32 -0500

    completed step 3: picture, name, city, bio and folowers are displaying to page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
-
-
-
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {
-    user:{},
+    user: {},
+    followers: {},
     active: false
-  }
+  };
 
-  
-  
-  // async handleClick(event) {
-  //   let user = await fetch('https://api.github.com/users/jayaimzzz');
-  //   user = await user.json();
-  //   console.log(user)
-  //   this.setState()
-  //   // console.log(this.state)
-  // }
-  handleClick = async (event) => {
-    let user = await fetch('https://api.github.com/users/jayaimzzz');
+  handleClick = async event => {
+    let user = await fetch("https://api.github.com/users/jayaimzzz");
     user = await user.json();
-    this.setState({user:user})
-  }
-  
+    let followers = await fetch(user.followers_url);
+    followers = await followers.json();
+    console.log(followers);
+    this.setState(state => ({
+      user: user,
+      followers: followers,
+      active: !state.active
+    }));
+  };
+
   render() {
     return (
       <div className="App">
         <button onClick={this.handleClick}>Button</button>
+        {this.state.active && (
+          <div>
+            <img src={this.state.user.avatar_url} key={this.state.user.id} />
+            <span>{this.state.user.name}</span>
+            <br/>
+            <span>{this.state.user.location}</span>
+            <br/>
+            <span>{this.state.user.bio}</span>
+            <div id="followers">
+              <h3>Followers</h3>
+              {this.state.followers.map(follower => (
+                <div>
+                  <img src={follower.avatar_url} key={follower.id} />
+                  <p>{follower.login}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
