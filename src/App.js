33f4087252a485d5bb949982d489bc4fe6f68e8e
James Horton <jameshorton@gmail.com>
2018-11-12 12:38:17 -0500

    completed part 2: fetch user and update state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 
 
 class App extends Component {
+  state = {
+    user:{},
+    active: false
+  }
+
   
-  handleClick = evt =>{
-    console.log("hello")
+  
+  // async handleClick(event) {
+  //   let user = await fetch('https://api.github.com/users/jayaimzzz');
+  //   user = await user.json();
+  //   console.log(user)
+  //   this.setState()
+  //   // console.log(this.state)
+  // }
+  handleClick = async (event) => {
+    let user = await fetch('https://api.github.com/users/jayaimzzz');
+    user = await user.json();
+    this.setState({user:user})
   }
   
   render() {
